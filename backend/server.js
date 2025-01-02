55ad5bba6303487f2630934ce009252f3901adf1
anshi777 <anshitav28@gmail.com>
2025-01-02 13:58:43 +0530

    som fix

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+// app.use(cors());
+// const cors = require('cors');
+app.use(cors({ origin: 'http://localhost:5173' })); 
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
